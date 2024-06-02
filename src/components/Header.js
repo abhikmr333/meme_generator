38bb5fe04151666900acb8cc98e376c196868c08
abhikmr333 <abhi363642@gmail.com>
2024-06-02 16:31:22 +0530

    changed className of header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import React from "react";
 
 export default function Header() {
     return (
-        <header className="Header">
+        <header className="header">
             <img
-                className="Header--trollimg"
+                className="header--trollimg"
                 src="images/troll_face.png"
                 alt=""
             />
-            <h1 className="Header--name">Meme Generator</h1>
-            <p className="Header--project">React Courses - Project 3</p>
+            <h1 className="header--name">Meme Generator</h1>
+            <p className="header--project">React Courses - Project 3</p>
         </header>
     );
 }
