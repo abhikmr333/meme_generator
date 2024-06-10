668a0f1bdd1cde2a0ced4f328a0898f4d1059377
abhikmr333 <abhi363642@gmail.com>
2024-06-10 20:25:49 +0530

    connected the inputs to state and made them visible over the image plus inputs are controlled inputs

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,17 +3,41 @@ import React, { useState } from "react";
 import memesData from "../memesData.js";
 
 export default function Meme() {
-    //new state / state variable with initial value as empty string
-    const [memeImage, setMemeImage] = useState("");
+    //new state with initial value as meme object
+    //also connecting inputs with state
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/1bij.jpg",
+    });
+
+    //new state which defaults to the imported memesData above
+    //why this one in state? Assuming more memes get added later down the line using setAllMemeImages
+    const [allMemeImages, setAllMemeImages] = useState(memesData);
 
     function getMemeImage() {
-        const memesArr = memesData.data.memes;
-        const random = Math.floor(Math.random() * 100);
-        const url = memesArr[random].url;
-        //call the setter function
-        setMemeImage(url);
+        //extract a random image when button clicked
+        const memesArray = allMemeImages.data.memes;
+        const random = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[random].url;
+
+        //and call the setter function to update image
+        setMeme((prevMeme) => {
+            return {
+                ...prevMeme,
+                randomImage: url,
+            };
+        });
     }
 
+    //function that handles the changes on each keystorke in top and bottom inputs
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setMeme((prevMeme) => {
+            return { ...prevMeme, [name]: value };
+        });
+    }
+    console.log(meme.topText + " " + meme.bottomText);
     return (
         <main className="Meme">
             <section className="Meme--input-section">
@@ -21,7 +45,15 @@ export default function Meme() {
                     <label htmlFor="top" className="Meme--top-label">
                         Top text
                     </label>
-                    <input id="top" type="text" className="Meme--top-field" />
+                    <input
+                        name="topText"
+                        //for controlled inputs
+                        value={meme.topText}
+                        onChange={handleChange}
+                        id="top"
+                        type="text"
+                        className="Meme--top-field"
+                    />
                 </div>
 
                 <div className="bottom">
@@ -29,6 +61,10 @@ export default function Meme() {
                         Bottom text
                     </label>
                     <input
+                        name="bottomText"
+                        //for controlled inputs
+                        value={meme.bottomText}
+                        onChange={handleChange}
                         id="bottom"
                         type="text"
                         className="Meme--bottom-field"
@@ -37,8 +73,6 @@ export default function Meme() {
             </section>
 
             <button
-                //attaching event listeners
-                //since this is a proper form remember to use event.preventDefault()
                 onClick={getMemeImage}
                 className="Meme--button"
                 type="submit"
@@ -50,8 +84,10 @@ export default function Meme() {
                     className="Meme--button-image"
                 />
             </button>
-            <div className="Meme--image-container">
-                <img className="Meme--image" src={memeImage} />
+            <div className="Meme--container">
+                <img className="Meme--image" src={meme.randomImage} />
+                <h2 className="Meme--top-text">{meme.topText}</h2>
+                <h2 className="Meme--bottom-text">{meme.bottomText}</h2>
             </div>
         </main>
     );
